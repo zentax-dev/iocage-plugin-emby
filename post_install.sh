00e6289c405b0e5ac2564bd09b519941ad51348f
Lars Schmidt <syntaks@posteo.eu>
2025-10-03 19:11:09 +0200

    get emby updated in freenas 13.3

--- post_install.sh
+++ post_install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+pkg add -f https://github.com/MediaBrowser/Emby.Releases/releases/download/4.9.1.36/emby-server-freebsd13_4.9.1.36_amd64.pkg
 # Enable the service
 sysrc -f /etc/rc.conf emby_server_enable="YES"
 
